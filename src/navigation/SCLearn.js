2b984b5531bc1be4931a487c9345a14a475a9d14
Anshuman <anshumanmishra.v.2.8@gmail.com>
2025-05-12 08:58:51 +0530

    github build adjustment

--- src/navigation/SCLearn.js
+++ src/navigation/SCLearn.js
@@ -1,12 +1,12 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { PaperProvider } from "react-native-paper";
-import Home from "../screens/SCB/SCLearnhome";
-const Stack = createNativeStackNavigator();
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { PaperProvider } from "react-native-paper";
+// import Home from "../screens/SCB/SCLearnhome";
+// const Stack = createNativeStackNavigator();
 
-export default function SCLearn() {
-  return (
-    <PaperProvider>
-      <Home />
-    </PaperProvider>
-  );
-}
+// export default function SCLearn() {
+//   return (
+//     <PaperProvider>
+//       <Home />
+//     </PaperProvider>
+//   );
+// }
